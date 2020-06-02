2677e4643712ed690fdec5207e37cd671ca1b585
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-06-01 21:15:42 -0400

    Único commit.

--- ASPNETjqueryAjax/Controllers/HomeController.cs
+++ ASPNETjqueryAjax/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASPNETjqueryAjax.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,56 @@ namespace ASPNETjqueryAjax.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult PruebaAjax() {
+            return View();
+        }
+        public ActionResult Ejemplo1() //Queremos llegar acá usando ajax
+        {
+            return Content("Texto retorado desde la Acción Ejemplo1", "text/plain");
+        }
+        public ActionResult Ejemplo2(string nombreC)//Recibir un dato y enviar otro, previo proceso
+        {
+            return Content("Hola, bienvenido "+ nombreC, "text/plaint");
+        }
+        public ActionResult Ejemplo3()//envía un objeto a la página
+        {
+            Producto p = new Producto() {
+                Id = "001",
+                Nombre = "Sardinas",
+                Precio = 12.5f
+            };
+            return Json(p,JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Ejemplo4()//Envía una lista de objetos a la página
+        {
+            List<Producto> productos = new List<Producto>() {
+                new Producto()
+            {
+                Id = "001",
+                Nombre = "Sardinas",
+                Precio = 12.5f
+            },
+                new Producto()
+            {
+                Id = "002",
+                Nombre = "Atún",
+                Precio = 10f
+            },
+                new Producto()
+            {
+                Id = "003",
+                Nombre = "Pan",
+                Precio = 5f
+            },
+                new Producto()
+            {
+                Id = "004",
+                Nombre = "Azúcar",
+                Precio = 8.5f
+            }
+            };
+            return Json(productos, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ ASPNETjqueryAjax/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETjqueryAjax.Models
+{
+    public class Producto
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+    }
+}
